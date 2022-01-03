567941b79ef314c9172503c3e31e8466c5571423
Tianjiao Huang <git@tjhu.dev>
2022-01-03 14:17:05 -0500

    report: support running with different n_threads

--- report.py
+++ report.py
@@ -10,6 +10,7 @@ import subprocess
 import csv
 import pandas as pd
 import multiprocessing
+import math
 
 # Initialize logging
 logger = logging.getLogger()
@@ -23,12 +24,21 @@ logger.addHandler(file_handler)
 # Parse arguments
 DEFAULT_TEST_SIZE = 2 ** 26
 DEFAULT_OUTPUT_DIR = f"./report/{datetime.now().strftime('%Y_%d_%m_%H_%M_%S')}"
+DEFAULT_THREADS = [2 ** i for i in range(int(math.log(multiprocessing.cpu_count(), 2))+ 1)] # [1, 2, 4, ..., N_CPU]
+DEFAULT_CATEGORIES = [
+  'ins',
+  'mix',
+  'agg',
+  'con',
+  'del',
+]
 parser = argparse.ArgumentParser()
 parser.add_argument("--input", help="path to the directories containing all the test binaries.", default="./build")
 parser.add_argument("--output", help="output directory.", default=DEFAULT_OUTPUT_DIR)
 parser.add_argument("-n", "--test-size", help="number of operations(insert/delete) per test.", default=DEFAULT_TEST_SIZE, type=int)
-parser.add_argument("-l", "--load-factor", help="hashtable capacity", default=0.75, type=float)
-parser.add_argument("-p", "--num_threads", help="number of threads", default=multiprocessing.cpu_count(), type=float)
+parser.add_argument("-l", "--load-factor", help="hashtable load factor", default=0.75, type=float)
+parser.add_argument("-t", "--num_threads", help="number of threads", default=DEFAULT_THREADS, type=int, nargs='+')
+parser.add_argument("-c", "--test_categories", help="test categories to run", default=DEFAULT_THREADS, type=str, nargs='+')
 parser.add_argument("--log", help="log level", default="INFO")
 args = parser.parse_args()
 logging.debug(f"Program arguments: {args}")
@@ -47,15 +57,8 @@ if not os.path.isdir(args.output):
   pathlib.Path(args.output).mkdir(parents=True, exist_ok=True)
 
 # Run all tests and generate reports.
-logging.info(f"Running tests with size {args.test_size}, capacity {capacity}, and {args.num_threads} threads.")
-test_categories = [
-  'ins',
-  'mix',
-  'agg',
-  'con',
-  'del',
-]
-for category in test_categories:
+logging.info(f"Running tests with categories {args.test_categories}, size {args.test_size}, capacity {capacity}, and {args.num_threads} threads.")
+for category in args.test_categories:
   test_dir = path.join(args.input, category)
   if path.isdir(test_dir):
     logging.info(f"Running tests in category <{category}>")
@@ -70,23 +73,24 @@ for category in test_categories:
   results = [] # Lists of KV pairs results which will be written the a CSV.
   column_names = {'name'}
   for test in test_files:
-    # Run test.
-    logging.info(f"Running test <{test}>.")
-    outfile_path = path.join(output_dir, f"{test}.log")
-    with open(outfile_path, 'w') as outfile:
-      subprocess.run([path.join(test_dir, test), '-c', str(capacity), '-n', str(args.test_size), '-p', str(args.num_threads)], stdout=outfile).check_returncode()
+    for n_threads in args.num_threads:
+      # Run test.
+      logging.info(f"Running test <{test}> with {n_threads} threads.")
+      outfile_path = path.join(output_dir, f"{test}_{n_threads}.log")
+      with open(outfile_path, 'w') as outfile:
+        subprocess.run([path.join(test_dir, test), '-c', str(capacity), '-n', str(args.test_size), '-p', str(n_threads)], stdout=outfile).check_returncode()
 
-    # Parse output.
-    logging.debug(f"Parsing test <{test}> output.")
-    with open(outfile_path, 'r', newline='') as outfile:
-      output_csv = pd.read_csv(outfile, sep='\s+')
-      columns = list(filter(lambda s: s.startswith('t_'), output_csv.columns))
-      logging.debug(f"Found columns {columns} for <{test}>")
-      column_names.update(columns)
-      result = {column: output_csv[column].mean() for column in columns}
-      result['name'] = test
-      results.append(result)
-    logging.info(f"Finished running test <{test}>.")
+      # Parse output.
+      logging.debug(f"Parsing test <{test}> output.")
+      with open(outfile_path, 'r', newline='') as outfile:
+        output_csv = pd.read_csv(outfile, sep='\s+')
+        columns = list(filter(lambda s: s.startswith('t_'), output_csv.columns))
+        logging.debug(f"Found columns {columns} for <{test}>")
+        column_names.update(columns)
+        result = {column: output_csv[column].mean() for column in columns}
+        result['name'] = test
+        results.append(result)
+      logging.info(f"Finished running test <{test}>.")
 
   # Write results to CSV
   output_report_path = path.join(output_dir, f'{category}.csv')
